acbd4892bd35f76ab36a8da01a0695f64aae372e
namnguyen28112007-dot <namnguyen28112007@gmail.com>
2026-09-26 04:48:42 +0700

    Update ExamRandom.cpp

--- ExamRandom.cpp
+++ ExamRandom.cpp
@@ -22,9 +22,7 @@ int layCauHoiVaoMang(
     }
     return n;
 } 
-void tronCauHoi(
-    CauHoi* ds[],
-    int n){
+void tronCauHoi(CauHoi* ds[], int n){
         for (int i=n-1;i>0;i--){
             int j=rand()%(i+1);
             hoanVi(ds[i],ds[j]);
