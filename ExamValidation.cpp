17dc2db4d13a7387ae97aee8c28c885cc7d9cd4c
namnguyen28112007-dot <namnguyen28112007@gmail.com>
2026-09-26 04:23:59 +0700

    Update ExamValidation.cpp

--- ExamValidation.cpp
+++ ExamValidation.cpp
@@ -1,12 +1,22 @@
 #include "ExamValidation.h"
-#include <cctype>
+char chuanHoaDapAn(char dapAn){
+    if(dapAn >= 'a' && dapAn <= 'd'){
+        dapAn = dapAn - 'a' + 'A';
+    }
+    return dapAn;
+}
 bool kiemTraDapAn(char dapAn){
-    dapAn=toupper(dapAn);
-    return dapAn == 'A'||dapAn == 'B'||dapAn =='C'||dapAn =='D';
+    dapAn = chuanHoaDapAn(dapAn);
+
+    return dapAn >= 'A' && dapAn <= 'D';
 }
 double tinhDiem(int soCauDung, int tongSoCau){
-    if(tongSoCau<=0){
+    if(tongSoCau <= 0){
         return 0.0;
     }
-    return (double)soCauDung/tongSoCau*10.0;
+    if(soCauDung < 0 || soCauDung > tongSoCau){
+        return 0.0;
+    }
+    return static_cast<double>(soCauDung)
+           / tongSoCau * 10.0;
 }
