4b3bc11bc695f2debb2beb9c9fd21f60e52d59ad
namnguyen28112007-dot <namnguyen28112007@gmail.com>
2026-09-26 04:38:15 +0700

    Update ExamHistory.h

--- ExamHistory.h
+++ ExamHistory.h
@@ -1,22 +1,8 @@
 #pragma once
 #include "models.h"
-ChiTietThi* taoChiTietThi(
-    CauHoi* cauHoi,
-    char dapAnSV
-);
-void themChiTietThi(
-    BaiThi* baiThi,
-    ChiTietThi* moi
-);
-BaiThi* taoBaiThi(
-    const std::string&maMH,
-    int soCau
-);
-void themBaiThiVaoLichSu(
-    SinhVien* sv,
-    BaiThi* baiThi
-);
-BaiThi* timBaiThi(
-    SinhVien* sv,
-    const std::string&maMH
-);
+ChiTietThi* taoChiTietThi( CauHoi* cauHoi, char dapAnSV);
+void themChiTietThi(BaiThi* baiThi, ChiTietThi* moi);
+BaiThi* taoBaiThi(const std::string&maMH, int soCau);
+void themBaiThiVaoLichSu( SinhVien* sv, BaiThi* baiThi);
+BaiThi* timBaiThi( SinhVien* sv, const std::string&maMH);
+void chamBaiThi(SinhVien* sv, BaiThi* baiThi);
